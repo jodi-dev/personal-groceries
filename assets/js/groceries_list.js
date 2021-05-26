553c15423e966ed3822f22c9503a9673ea08cf42
Jodi <dqiao100@uottawa.ca>
2021-05-25 22:34:03 -0400

    more work on allergy filter showing the filtered products to shop

--- assets/js/groceries_list.js
+++ assets/js/groceries_list.js
@@ -1,28 +1,27 @@
 // name, price, allergen, bool
 var all_items = [
-    ['Almond milk', '$5', 'nuts'],
-    ['Milk', '$4', 'dairy'],
-    ['Diet coke', '$2', 'aspartame'],
-    ['Strawberries', '$5', 'berries'],
-    ['Granola bars', '$6', 'nuts']
+    ['Almond milk', '5', 'nuts'],
+    ['Milk', '4', 'dairy'],
+    ['Diet coke', '2', 'none'],
+    ['Strawberries', '5', 'berries'],
+    ['Granola bars', '6', 'nuts'],
+    ['Blueberries', '4', 'berries'],
+    ['Bread', '2', 'gluten'],
+    ['Cereal', '5', 'gluten'],
+    ['Cheese', '7', 'dairy'],
+    ['Beef', '12', 'none']
 ]
+
+var filtered_items;
+
+var minPrice = 0;
+var maxPrice = 100;
 
 var allergiesChecked = [];
 
 $(document).ready( function filterProds() {
-    // allergies list
-    // var nuts = document.getElementsByName("nuts");
-    // var dairy = document.getElementsByName("dairy"); 
-    // var berries = document.getElementsByName("berries");
+    // document change listeners
 
-    // $("#apply").click(function() {
-    //     $.each($("input[name='allergy']:checked"), function() {
-    //         if (! allergiesChecked.includes($(this).val())) {
-    //             allergiesChecked.push($(this).val());
-    //         }
-    //     });
-    //     alert("my allergies are: " + allergiesChecked.join(","));
-    // });
     $('#nuts').change(function() {
         if(this.checked) {
             //alert('nuts allergy');
@@ -72,10 +71,35 @@ $(document).ready( function filterProds() {
         }
     });
 
+    // min price update
+    $('#min-price').change(function() {
+        minPrice = $(this).val();
+    });
+
+    // max price update
+    $('#max-price').change(function() {
+        maxPrice = $(this).val();
+    });
 
     // apply selections
     $('#apply').click(function() {
+        let filtered = [];
+        let v;
         alert("my allergies are: " + allergiesChecked.join(","));
+        for (let i = 0; i<all_items.length; i+=1) {
+            v = true;
+            for (let j = 0; j<allergiesChecked.length; j+=1) {
+                if (all_items[i][2] == allergiesChecked[j]) {
+                    v = false;
+                }
+            }
+            if (v == true) {
+                filtered.push(all_items[i]);
+            }
+            //filtered.push(all_items[i]);
+        }
+        alert("my items are: " + filtered.join(","));
+        filtered_items = filtered;
     });
 
     // clear selections
@@ -83,6 +107,8 @@ $(document).ready( function filterProds() {
         document.getElementById("nuts").checked = false;
         document.getElementById("dairy").checked = false;
         document.getElementById("berries").checked = false;
+        minPrice = 0;
+        maxPrice = 100;
         allergiesChecked = [];
         alert("All selections cleared");
     });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
     let content = '';
-    all_items.forEach(p=> {
-        content += 
-        `
-        <div class="card"><img class="card-img-top w-100 d-block">
-            <div class="card-body">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="">
-                        <label class="form-check-label" for="formCheck-1"></label>
+
+    if (allergiesChecked.length == 0) {
+        all_items.forEach(p=> {
+            content += 
+            `
+            <div class="card"><img class="card-img-top w-100 d-block">
+                <div class="card-body">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="">
+                            <label class="form-check-label" for="formCheck-1"></label>
+                    </div>
+                    <h4 class="card-title">${p[0]}</h4>
+                    <p class="card-text">$ ${p[1]}</p>
                 </div>
-                <h4 class="card-title">${p[0]}</h4>
-                <p class="card-text">${p[1]}</p>
-            </div>
-        </div>`
-    });
-    document.getElementById("shop").innerHTML = content;
+            </div>`
+        });
+        document.getElementById("shop").innerHTML = content;
+    } 
+    document.getElementById("min-price").value = minPrice;
+    document.getElementById("max-price").value = maxPrice;
 });
+
+function updateShop(listProds, ) {
+    // updateShop function to replace the rendering of DOM with all_items cards
+    
+}
